d2c15d3efad0edfbe9cdbf9844992a664cc9d269
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2024-11-05 18:12:27 +1100

    Updated supervisor

--- run_base.sh
+++ run_base.sh
@@ -46,10 +46,10 @@ docker run -d --name "robotic_supervisor" \
     -e WATCHTOWER_INCLUDE_RESTARTING=true \
     -e WATCHTOWER_HTTP_API_TOKEN=robotics \
     -e WATCHTOWER_HTTP_API_PERIODIC_POLLS=true \
-    -e DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket \
     -p 8080:8080 \
-    --mount type=bind,source="$(pwd)"/config,target=/config \
-    --mount type=bind,source=/run/dbus/system_bus_socket,target=/run/dbus/system_bus_socket \
-    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+    -v "$(pwd)"/config:/config \
+    -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /proc:/proc \
     --label=com.centurylinklabs.watchtower.enable=false \
     dkhoanguyen/robotic_supervisor:latest --interval 300 --http-api-update --port 8080 --update-on-startup
